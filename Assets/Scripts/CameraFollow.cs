3e2ab7531fb88cc007e6e810ad1a1204f1f2a723
trihai264 <53052320+trihai264@users.noreply.github.com>
2021-04-17 18:30:37 +0300

    Added CameraFollow

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public float dampTime = 10f;
+    public Vector3 cameraPos = Vector3.zero;
+    public GameObject target;
+    public float xOffset = 0;
+    public float yOffset = 0;
+    public float otherMargin;
+    public float camSpeed;
+    public float maxCamSpeed;
+    public float camAcc;
+    public float margin = 0.1f;
+
+    // Use this for initialization
+    void Start()
+    {
+        target = GameObject.FindWithTag("Player");
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+
+        if (target != null)
+        {
+            cameraPos = new Vector3(Mathf.SmoothStep(transform.position.x, target.transform.position.x, dampTime), Mathf.SmoothStep(transform.position.y, target.transform.position.y, dampTime));
+        }
+
+
+
+    }
+    void LateUpdate()
+    {
+
+        transform.position = cameraPos + Vector3.forward * -10;
+
+
+    }
+}
