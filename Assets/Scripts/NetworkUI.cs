994ca5cd448627acd877410b98aadcd07a1ce2e1
Jyri Puputti <jyri.puputti@ruskatek.com>
2021-04-17 19:49:52 +0300

    ...

--- Assets/Scripts/NetworkUI.cs
+++ Assets/Scripts/NetworkUI.cs
@@ -4,41 +4,136 @@ using UnityEngine;
 using UnityEngine.UI;
 using MLAPI;
 using MLAPI.Transports.UNET;
+using MLAPI.Connection;
 
 public class NetworkUI : MonoBehaviour
 {
+    static string ErrorNameTooShort = "Name needs to be more than 3 characters long";
     // Start is called before the first frame update
     Button buttonHost;
     Button buttonJoin;
     InputField inputFieldIp;
+    InputField inputFieldName;
+    Text errorText;
+    float t = 0.0f;
     void Start()
     {
         buttonHost = GameObject.Find ("Host").GetComponent<Button>();
         buttonJoin = GameObject.Find ("Join").GetComponent<Button>();
         inputFieldIp = GameObject.Find ("InputFieldIP").GetComponent<InputField>();
-
+        inputFieldName = GameObject.Find ("InputFieldName").GetComponent<InputField>();
+        errorText = GameObject.Find ("Error").GetComponent<Text>();
+        errorText.text = "";
         buttonHost.onClick.AddListener (Host);
         buttonJoin.onClick.AddListener (Join);
+
+        NetworkManager.Singleton.OnServerStarted += ServerStarted;
+        NetworkManager.Singleton.OnClientDisconnectCallback += ClientDisconnected;
+        NetworkManager.Singleton.OnClientConnectedCallback += ClientConnected;
     }
 
-    // Update is called once per frame
-    void Host()
+	private void OnDestroy()
+	{
+		NetworkManager.Singleton.OnServerStarted -= ServerStarted;
+        NetworkManager.Singleton.OnClientDisconnectCallback -= ClientDisconnected;
+        NetworkManager.Singleton.OnClientConnectedCallback -= ClientConnected;
+	}
+
+	private void Update()
+	{
+		if (gameObject.activeInHierarchy == true && (NetworkManager.Singleton.IsClient))
+        {
+            if (Time.time - t >= 5.0f)
+            {
+                errorText.text = "Failed to Join";
+                NetworkManager.Singleton.StopClient ();
+			}
+		}
+	}
+
+	// Update is called once per frame
+	void Host()
     {
-        NetworkManager.Singleton.StartHost ();
-        gameObject.SetActive(false);
+        if (NetworkManager.Singleton.IsClient)
+        {
+            NetworkManager.Singleton.StopClient ();
+		}
+
+        else if (NetworkManager.Singleton.IsHost)
+        {
+            
+            NetworkManager.Singleton.StopHost ();
+		}
+
+        if (inputFieldName.text.Length < 3)
+        {
+            errorText.text = ErrorNameTooShort;
+		}
+
+        else
+        { 
+            errorText.text = "";
+            NetworkManager.Singleton.StartHost ();
+            gameObject.SetActive(false);
+        }
     }
 
     void Join()
     {
-        if (inputFieldIp.text.Length > 0)
+        if (NetworkManager.Singleton.IsClient)
         {
-            NetworkManager.Singleton.GetComponent<UNetTransport>().ConnectAddress = inputFieldIp.text;
+            NetworkManager.Singleton.StopClient ();
 		}
+
+        else if (NetworkManager.Singleton.IsHost)
+        {
+            
+            NetworkManager.Singleton.StopHost ();
+		}
+
+        if (inputFieldName.text.Length < 3)
+        {
+            errorText.text = ErrorNameTooShort;
+		}
+
         else
         {
-            NetworkManager.Singleton.GetComponent<UNetTransport>().ConnectAddress = "127.0.0.1";
-		}
-        NetworkManager.Singleton.StartClient ();
-        gameObject.SetActive(false);
+            errorText.text = "Connecting...";
+            if (inputFieldIp.text.Length > 0)
+            {
+                NetworkManager.Singleton.GetComponent<UNetTransport>().ConnectAddress = inputFieldIp.text;
+		    }
+            else
+            {
+                NetworkManager.Singleton.GetComponent<UNetTransport>().ConnectAddress = "127.0.0.1";
+		    }
+
+            NetworkManager.Singleton.StartClient ();
+            t = Time.time;
+        }
     }
+
+    void ServerStarted ()
+    {
+        if (NetworkManager.Singleton.IsServer)
+        {
+            gameObject.SetActive(false);
+		}
+	}
+    void ClientConnected (ulong v)
+    {
+        if (!NetworkManager.Singleton.IsServer)
+        {
+            gameObject.SetActive(false);
+		}
+	}
+
+    void ClientDisconnected (ulong v)
+    {
+        if (!NetworkManager.Singleton.IsServer)
+        {
+            gameObject.SetActive(true);
+            errorText.text = "Disconnected from the Host";
+		}
+	}
 }
